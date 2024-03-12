fd25982cb903a6e9e738f9f21b98e1bda1f36708
Andrey <antonov.andrey.10101@gmail.com>
2024-03-12 16:35:39 +0300

    feat: add getting cards

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,25 @@
-function App() {
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+import { addToCart, updateQuantity, removeFromCart } from './redux/cartSlice';
+import Cart from './Cart';
+import Total from './Total';
+import { useAppDispatch, useAppSelector } from '../../hooks/useRedux';
+import { fetchCards } from '../../slices/cardsSlice';
 
-  return (
-    <div className="app">
-      Testing
-    </div>
-  );
+function App() {
+  const dispatch = useAppDispatch();
+  const { cards, cardsLoadingStatus } = useAppSelector((state) => state.cards);
+
+  useEffect(() => {
+    dispatch(fetchCards('https://fakestoreapi.com/products'));
+  }, []);
+
+  if (cardsLoadingStatus === 'loading') {
+    return <p>Loading...</p>;
+  }
+
+  return <div className="app">test</div>;
 }
 
 export default App;
--- src/slices/cardsSlice.ts
+++ src/slices/cardsSlice.ts
@@ -1,44 +1,42 @@
-import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-
-type 
-
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { ICard } from '../types';
 
 export type CardsState = {
-    cards: [],
-    cardsLoadingStatus: 'loading',
+    cards: ICard[];
+    cardsLoadingStatus: 'loading' | 'idle' | 'error';
 };
 
 const initialState: CardsState = {
     cards: [],
-    cardsLoadingStatus: 'loading',
+    cardsLoadingStatus: 'idle',
 };
 
-export const fetchCards = createAsyncThunk('cards/fetchCards', (url: string) => {
-
+export const fetchCards = createAsyncThunk('cards/fetchCards', async (url: string) => {
+    const response = await axios.get(url);
+    return response.data;
 });
-
 
 const cardsSlice = createSlice({
     name: 'cards',
     initialState,
-    reducers: {
-
-    },
+    reducers: {},
     extraReducers: (builder) => {
         builder
             .addCase(fetchCards.pending, (state) => {
-                // state.cardsLoadingStatus = 'loading';
+                state.cardsLoadingStatus = 'loading';
             })
             .addCase(fetchCards.fulfilled, (state, action) => {
-                // state.cardsLoadingStatus = 'idle';
-                // state.cards = action.payload;
+                state.cardsLoadingStatus = 'idle';
+                state.cards = action.payload;
             })
             .addCase(fetchCards.rejected, (state) => {
-                // state.cardsLoadingStatus = 'error';
-            })
+                state.cardsLoadingStatus = 'error';
+            });
     },
 });
 
-const { reducer } = cardsSlice;
+export const { reducer } = cardsSlice;
 
 export default reducer;
