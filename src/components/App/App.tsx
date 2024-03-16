68d06d490dd790f74b3f5538671483f790c769ec
Andrey <antonov.andrey.10101@gmail.com>
2024-03-16 10:11:22 +0300

    feat: add padding

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -28,29 +28,25 @@ function App() {
 
   return (
     <AppRoot mode="embedded">
-      <SplitLayout>
-        <SplitCol autoSpaced>
-          <View activePanel="main">
-            <Panel id="main">
-              <PanelHeader>
-                <Div style={{ display: 'flex', alignItems: 'center', gap: '5px', padding: '20px' }}>
-                  <Icon56LogoVkColor width={50} />
-                  <Title level="1"> VK Маркет</Title>
-                </Div>
-              </PanelHeader>
-              {productsLoadingStatus === 'idle' && (
-                <Group header={<Header mode="secondary">Корзина товаров</Header>}>
-                  <Div style={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'space-between' }}>
-                    <ProductList />
-                    <Div style={{ flex: '1 1 25%' }}>{products.length ? <Total /> : ''}</Div>
-                  </Div>
-                </Group>
-              )}
-              {productsLoadingStatus === 'loading' && <Spinner size="large" />}
-            </Panel>
-          </View>
-        </SplitCol>
-      </SplitLayout>
+      <View activePanel="main">
+        <Panel id="main" style={{padding: '20px 15px'}}>
+          <PanelHeader>
+            <Div style={{ display: 'flex', alignItems: 'center', gap: '5px', padding: '20px' }}>
+              <Icon56LogoVkColor width={50} />
+              <Title level="1"> VK Маркет</Title>
+            </Div>
+          </PanelHeader>
+          {productsLoadingStatus === 'idle' && (
+            <Group header={<Header mode="secondary">Корзина товаров</Header>}>
+              <Div style={{ display: 'flex', alignItems: 'flex-start', justifyContent: 'space-between' }}>
+                <ProductList />
+                <Div style={{ flex: '1 1 25%' }}>{products.length ? <Total /> : ''}</Div>
+              </Div>
+            </Group>
+          )}
+          {productsLoadingStatus === 'loading' && <Spinner size="large" />}
+        </Panel>
+      </View>
     </AppRoot>
   );
 }
